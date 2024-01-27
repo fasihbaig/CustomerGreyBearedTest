2a877ee01dea8499d35884ddc25980ac3a422c06
fasihbaig <fasihbaig85@gmail.com>
2024-01-27 16:13:57 +0500

    Updated service to handle update and delete scenarios

--- Backend/services/CustomerService.cs
+++ Backend/services/CustomerService.cs
@@ -1,5 +1,8 @@
 ﻿using CustomerTest.Contexts;
+using CustomerTest.DTOs.Customer;
+using CustomerTest.Exceptions;
 using CustomerTest.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 
@@ -19,19 +22,44 @@ namespace CustomerTest.services
         public async Task<Customer> Create(Customer customer)
         {
             var result = await this._dbContext.Customers.AddAsync(customer);
-            this._dbContext.SaveChanges();
+            await this._dbContext.SaveChangesAsync();
             return result.Entity;
         }
 
-        public async void Delete(int customerId)
+        public async Task<Customer> Update(Guid id, UpdateCustomerDTO customer)
         {
-            var customer = await this._dbContext.Customers.FindAsync(customerId);
+            var existingCustomer = await this._dbContext.Customers.FindAsync(id);
+            if (existingCustomer == null )
+            {
+                throw new NotFoundException("Unable to find customer with id");
+            }
+            if(!string.IsNullOrEmpty(customer.name))
+            {
+                existingCustomer.Name = customer.name;
+            }
+
+            if (!string.IsNullOrEmpty(customer.email))
+            {
+                existingCustomer.Email = customer.email;
+            }
+
+            if (!string.IsNullOrEmpty(customer.phoneNumber))
+            {
+                existingCustomer.PhoneNumber = customer.phoneNumber;
+            }
+            await this._dbContext.SaveChangesAsync();
+            return existingCustomer;
+        }
+
+        public async Task<int> Delete(Guid id)
+        {
+            var customer = await this._dbContext.Customers.FindAsync(id);
             if(customer == null)
             {
-                throw new Exception("Customer not found");
+                throw new NotFoundException("Customer not found");
             }
             this._dbContext.Remove(customer);
-            this._dbContext.SaveChanges();
+            return await this._dbContext.SaveChangesAsync();
         }
     }
 }
