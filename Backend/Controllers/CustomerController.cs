ea32144bb0dd5d65b7ccdceb807d52323f95505e
fasihbaig <fasihbaig85@gmail.com>
2024-01-27 16:14:36 +0500

    updated controller to handle update and delete requests

--- Backend/Controllers/CustomerController.cs
+++ Backend/Controllers/CustomerController.cs
@@ -1,12 +1,15 @@
 using CustomerTest.Contexts;
+using CustomerTest.DTOs.Customer;
+using CustomerTest.Exceptions;
 using CustomerTest.Models;
 using CustomerTest.services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Any;
 
 namespace CustomerTest.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/customers")]
     public class CustomerController : ControllerBase
     {
 
@@ -28,34 +31,76 @@ namespace CustomerTest.Controllers
             this.customerService = customerService;
         }
 
-        [HttpGet(Name = "/customers")]
+        [HttpGet]
+        [Route("all")]
         public async Task<IResult> All()
         {
             var customers = await this.customerService.GetAllCustomers();
             return TypedResults.Ok(customers);
         }
 
-        [HttpPost(Name = "/customers")]
-        public async Task<IActionResult> Post()
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> Add(CreateCustomerDTO customerDTO)
         {
             try
             {
                 var result = await this.customerService.Create(
                 new Customer
                 {
-                    ID = new Random().Next(1, 999999999),
+                    ID = new Guid(),
                     CreatedAt = new DateTime(),
-                    Name = "dfsdf",
-                    Email = "fas@gm.com"
+                    Name = customerDTO.name,
+                    Email = customerDTO.email,
+                    PhoneNumber = customerDTO.phoneNumber,
 
                 });
 
                 return CreatedAtRoute(new { id = result.ID }, result);
             } catch (Exception ex)
             {
-                return StatusCode(500, "Unable to create new customer");
+                return StatusCode(500, ex);
+            } 
+        }
+
+        [HttpPut]
+        [Route("update/{id}")]
+        public async Task<IActionResult> Update(string id, UpdateCustomerDTO customerDTO)
+        {
+            try
+            {
+                var result = await this.customerService.Update(Guid.Parse(id), customerDTO);
+
+                return CreatedAtRoute(new { id = result.ID }, result);
             }
-            
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                if(ex is NotFoundException) { 
+                    return StatusCode(404, ex.Message);
+                }
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public async Task<IResult> Delete(string id)
+        {
+            try
+            {
+                 await this.customerService.Delete(Guid.Parse(id));
+                return TypedResults.Ok(id);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                if (ex is NotFoundException)
+                {
+                    return (IResult)StatusCode(404, ex.Message);
+                }
+                return (IResult)StatusCode(500, ex.Message);
+            }
         }
     }
 }
