ccf40661e33ca34e49be8c7208d8e58094485e2f
fasihbaig <fasihbaig85@gmail.com>
2024-01-27 16:12:16 +0500

    Added new exception to handle not found

---
+++ Backend/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomerTest.Exceptions
+{
+    public class NotFoundException: Exception
+    {
+        public NotFoundException(string message)
+       : base(message)
+        {
+        }
+    }
+}
