95da362221f70e1777229cfc586a1ba55f0be155
fasihbaig <fasihbaig85@gmail.com>
2024-01-27 16:14:12 +0500

    Added dtos for update and create

---
+++ Backend/DTOs/Customer/CreateCustomerDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomerTest.DTOs.Customer
+{
+    public class CreateCustomerDTO
+    {
+        public required string name { get; set; }
+        public required string email { get; set; }
+        public required string phoneNumber { get; set; }
+    }
+}
---
+++ Backend/DTOs/Customer/UpdateCustomerDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomerTest.DTOs.Customer
+{
+    public class UpdateCustomerDTO
+    {
+        public string? name { get; set; }
+        public  string? email { get; set; }
+        public  string? phoneNumber { get; set; }
+    }
+}
